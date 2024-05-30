17f318f654a6ff9bf7ea36e1a20ef05068dea6c8
Aslı Soylu <117990814+asoylu35@users.noreply.github.com>
2024-05-31 01:12:57 +0300

    Add files via upload

---
+++ methodOverloading/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+     DortIslem dortIslem= new DortIslem();
+        System.out.println(dortIslem.topla(2,3));
+        System.out.println(dortIslem.topla2(2,3,5));
+
+    }
+}
